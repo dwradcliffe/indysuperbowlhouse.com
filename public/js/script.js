e2d50cb0be42de536475218e910a776e95528385
David Radcliffe <david@etchdev.com>
2011-12-08 01:20:25 -0500

    add photos

--- public/js/script.js
+++ public/js/script.js
@@ -1,6 +1,16 @@
 /* Author:
-
+    David Radcliffe
 */
+
+$(function(){
+	$('#slides').slides({
+		preload: true,
+		preloadImage: 'images/loading.gif',
+		play: 5000,
+		pause: 2500,
+		hoverPause: true
+	});
+});
 
 var map;
 function initialize() {
