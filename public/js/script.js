fd0e7d661b7d9dac575df7953e5c8eed4be53cff
David Radcliffe <david@etchdev.com>
2011-12-12 19:04:17 -0500

    update content

--- public/js/script.js
+++ public/js/script.js
@@ -15,11 +15,21 @@ $(function(){
 var map;
 function initialize() {
   var myOptions = {
-    zoom: 8,
-    center: new google.maps.LatLng(-34.397, 150.644),
+    zoom: 11,
+    center: new google.maps.LatLng(39.72758197531012,-86.1743356191406),
     mapTypeId: google.maps.MapTypeId.ROADMAP
   };
   map = new google.maps.Map(document.getElementById('map'), myOptions);
+  var zip = new google.maps.Marker({
+    position: new google.maps.LatLng(39.665309,-86.181475), 
+    map: map, 
+    title: "Superbowl House"
+  });
+  var stadium = new google.maps.Marker({
+    position: new google.maps.LatLng(39.760316,-86.161976), 
+    map: map, 
+    title: "Lucas Oil Stadium"
+  });
 }
 
 google.maps.event.addDomListener(window, 'load', initialize);
