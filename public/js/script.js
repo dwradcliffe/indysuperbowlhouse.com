c0d8ef56c802a4f74b3da287b53f00f42fd9c135
David Radcliffe <david@etchdev.com>
2011-12-12 20:28:39 -0500

    more content adjustments

--- public/js/script.js
+++ public/js/script.js
@@ -3,13 +3,14 @@
 */
 
 $(function(){
-	$('#slides').slides({
-		preload: true,
-		preloadImage: 'images/loading.gif',
-		play: 5000,
-		pause: 2500,
-		hoverPause: true
-	});
+  $('#slides').slides({
+    preload: true,
+    preloadImage: 'images/loading.gif',
+    play: 5000,
+    pause: 2500,
+    paginationClass: 'photo-pagination',
+    hoverPause: true
+  });
 });
 
 var map;
